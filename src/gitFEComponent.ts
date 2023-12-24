14c2ecd54ed0ac4a5e1ad593d42cd33364413f35
Mateus Molina <mateus.molinabr@gmail.com>
2023-12-24 23:40:00 +0100

    Implement commit effect on clicking button

--- src/gitFEComponent.ts
+++ src/gitFEComponent.ts
@@ -1,7 +1,7 @@
 import { GitHandler } from "./gitHandler";
-
 export class GitFEComponent {
 	private gitFEElement: HTMLElement;
+	private eventsDisabled = false;
 
 	constructor(
 		private parent: HTMLElement,
@@ -9,15 +9,7 @@ export class GitFEComponent {
 		private gitHandler: GitHandler
 	) {
 		this.gitFEElement = this.createOrUpdateElement();
-		this.gitFEElement.addEventListener("click", this.onClick.bind(this));
-		this.gitFEElement.addEventListener(
-			"mouseover",
-			this.onMouseOver.bind(this)
-		);
-		this.gitFEElement.addEventListener(
-			"mouseout",
-			this.onMouseOut.bind(this)
-		);
+		this.addEventListeners();
 	}
 
 	async update() {
@@ -39,22 +31,54 @@ export class GitFEComponent {
 	}
 
 	public updateText(text: string | number): void {
-		this.gitFEElement.style.display =
-			text === 0 || text === "" ? "none" : "";
+		if (text == 0) {
+			this.gitFEElement.textContent = "git";
+			this.gitFEElement.classList.remove("git-fe-component-changes");
+			this.eventsDisabled = true;
+			return;
+		}
+
+		this.gitFEElement.classList.add("git-fe-component-changes");
 		this.gitFEElement.textContent = text.toString();
+		this.eventsDisabled = false;
 	}
 
-	public onClick() {
+	async onClick() {
+		if (this.eventsDisabled) return;
+		this.eventsDisabled = true;
 		this.gitHandler.stageAll(this.repoPath);
-		this.gitHandler.commit(this.repoPath, "auto commit");
+		this.gitHandler.commit(
+			this.repoPath,
+			"Sync " + new Date().toLocaleString()
+		);
+		this.gitFEElement.className =
+			"git-fe-component git-fe-component-committed";
+		setTimeout(() => {
+			this.gitFEElement.classList.remove("git-fe-component-committed");
+		}, 3000);
+
 		this.update();
 	}
 
 	public onMouseOver() {
+		if (this.eventsDisabled) return;
 		this.updateText("+");
 	}
 
 	public onMouseOut() {
+		if (this.eventsDisabled) return;
 		this.update();
+	}
+
+	private addEventListeners() {
+		this.gitFEElement.addEventListener("click", this.onClick.bind(this));
+		this.gitFEElement.addEventListener(
+			"mouseover",
+			this.onMouseOver.bind(this)
+		);
+		this.gitFEElement.addEventListener(
+			"mouseout",
+			this.onMouseOut.bind(this)
+		);
 	}
 }
